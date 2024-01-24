e75223de64069de090f50952bba91d1bedb59b40
Skola <martabrita@gmail.com>
2024-01-24 15:44:37 +0200

    pirma_lapa

---
+++ masinmacisanas/skrape.py
@@ -0,0 +1,40 @@
+import requests
+import os
+from bs4 import BeautifulSoup as bs
+import csv
+import time
+
+
+URL = "https://www.ss.lv/lv/transport/cars/today-5/sell/"
+LAPAS = "lapas/"
+DATI = "dati/"
+
+def saglaba(url, datne):
+    rezultats = requests.get(url)
+    print(rezultats.status_code)
+    if rezultats.status_code == 200:
+        with open(datne, 'w', encoding='utf-8') as fails:
+            fails.write(rezultats.text)
+    return
+
+# saglaba(URL, LAPAS+"pirma.html")
+
+def dabut_info(datne):
+    dati = []
+    with open(datne, "r", encoding="utf-8") as f:
+        html = f.read()
+    
+    zupa = bs(html, 'html.parser')
+
+    galvena_dala = zupa.find(id='page_main')
+
+    tabulas = galvena_dala.find_all('table')
+
+    rindas = tabulas[1].find_all('tr')
+
+    print(rindas)
+
+
+
+
+
