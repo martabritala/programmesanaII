24379cbddbece96cae66919df11bd6e254374c00
martabritala <53839047+martabritala@users.noreply.github.com>
2024-02-16 14:21:18 +0200

    15.02. kods

--- majas/kods.py
+++ majas/kods.py
@@ -11,9 +11,31 @@ label = []
 bilzu_adrese = "majas/bildes/"
 
 for nosaukums in os.listdir(bilzu_adrese):
-    image = Image.open(os.path.join(bilzu_adrese,nosaukums))
+    image = Image.open(os.path.join(bilzu_adrese,nosaukums)).resize((200,200), Image.Resampling.NEAREST)
     bildes.append(np.array(image))
     if "maja" in nosaukums:
         label.append(1)
     else:
         label.append(0)
+
+
+#Jāpārtaisa par python skaitļu sarakstiem.
+bildes = np.array(bildes)
+label = np.array(label)
+
+bildes = bildes/255.0 #
+bildes = bildes.reshape(bildes.shape[0], -1)
+
+X_train, X_test, y_train, y_test = train_test_split(bildes, label, test_size=0.2)
+
+
+
+modelis = RandomForestClassifier()
+
+modelis.fit(X_train, y_train)
+
+paregojums = modelis.predict(X_test)
+
+precizitate = accuracy_score(y_test, paregojums)
+
+print(precizitate)
